2c8118c444e0c0f38ff6dfac8586f9b3caa60287
melvin <melvin.melvin.rulit@gmail.com>
2026-05-26 16:27:41 +0000

    файл конфигурации для разрешений

--- bc-cms/config/permissions.php
+++ bc-cms/config/permissions.php
@@ -15,24 +15,21 @@ return [
     'language_manage',
     'language_translation',
 
-
     // Booking
     'booking_view',
     'booking_update',
     'booking_manage_others',
 
-    // Booking
+    // Enquiry
     'enquiry_view',
     'enquiry_update',
     'enquiry_manage_others',
-
 
     // Templates
     'template_view',
     'template_create',
     'template_update',
     'template_delete',
-
 
     // News
     'news_view',
@@ -48,6 +45,7 @@ return [
     'role_update',
     'role_delete',
 
+    // Permission
     'permission_view',
     'permission_create',
     'permission_update',
@@ -55,11 +53,16 @@ return [
 
     // Dashboard
     'dashboard_access',
-    'dashboard_vendor_access',
+    'dashboard_baseAdmin_access',
+    'dashboard_guest_access',
 
     // Settings
-    'setting_update',
-
+    'settings_update',
+    'settings_view',
+    'settings_update',
+    'settings_delete',
+    'settings_manage_others',
+    'settings_manage_attributes',
 
     // Menus
     'menu_view',
@@ -67,25 +70,23 @@ return [
     'menu_update',
     'menu_delete',
 
-
     // create permissions
     'user_view',
     'user_create',
     'user_update',
     'user_delete',
 
+    // page
     'page_view',
     'page_create',
     'page_update',
     'page_delete',
     'page_manage_others',
 
-    'setting_view',
-    'setting_update',
-
     // Media
     'media_upload',
     'media_manage',
+    'media_manage_others',
 
     // Location
     'location_view',
@@ -95,13 +96,11 @@ return [
     'location_manage_others',
 
     //Review
-    'review_manage_others',
+    'review_manage',
 
     // Other System Permissions
-
     'system_log_view',
     'theme_manage',
-
 
     // Social
     'social_manage_forum',
@@ -120,47 +119,11 @@ return [
     'popup_update',
     'popup_delete',
 
-    // Base Admin
-    'baseAdmin_dashboard_access',
-    'baseAdmin_content',
-    'baseAdmin_service',
-
-    // Hunter
-    'hunter_dashboard_access',
-    'hunter_content',
-    'hunter_edit_events',
-
-    // Animals
-    'animal_view',
-    'animal_create',
-    'animal_update',
-    'animal_delete',
-    'animal_availability_dates',
-    'animal_create_hunting',
-
-    //Weapon
-    'weapon_view',
-    'weapon_create',
-
-    //Caliber
-    'caliber_view',
-    'caliber_create',
-
     //Attendance
-    'attendance_view',
-    'attendance_create',
-    'attendance_update',
-    'attendance_delete',
-    'attendance_manage_others',
-    'attendance_manage_attributes',
-
-    //Settings
-    'settings_view',
-    'settings_create',
-    'settings_update',
-    'settings_delete',
-    'settings_manage_others',
-    'settings_manage_attributes',
-
-    'media_manage_others'
+//    'attendance_view',
+//    'attendance_create',
+//    'attendance_update',
+//    'attendance_delete',
+//    'attendance_manage_others',
+//    'attendance_manage_attributes',
 ];
