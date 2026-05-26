4b9a0bccaa7a3f414a9a4e4f8101af2adb3c5ad6
melvin <melvin.melvin.rulit@gmail.com>
2026-05-26 16:20:19 +0000

    файл с переводами для групп разрешений и самих разрешений

---
+++ bc-cms/resources/lang/ru/permission.php
@@ -0,0 +1,129 @@
+<?php
+
+return [
+    'names' => [
+        'report' => 'Отчет',
+        'contact' => 'Контакты',
+        'newsletter' => 'Рассылка',
+        'language' => 'Язык и переводы',
+        'booking' => 'Бронирование',
+        'enquiry' => 'Заявки',
+        'template' => 'Шаблоны',
+        'news' => 'Новости',
+        'role' => 'Роли',
+        'permission' => 'Разрешения',
+        'dashboard' => 'Панель мониторинга',
+        'settings' => 'Настройки',
+        'menu' => 'Меню',
+        'user' => 'Пользователь',
+        'page' => 'Управление страницами',
+        'media' => 'Медиафайлы',
+        'location' => 'Локация',
+        'review' => 'Отзывы',
+        'system' => 'Системные',
+        'theme' => 'Оформление и темы',
+        'popup' => 'Всплывающие окна',
+        'hotel' => 'Отели',
+    ],
+    'permissions' => [
+        'report_view' => 'Просмотр отчетов',
+
+        'contact_manage' => 'Управление контактными заявками',
+
+        'newsletter_manage' => 'Управление рассылками',
+
+        'language_manage' => 'Управление языками',
+        'language_translation' => 'Управление переводами',
+
+        'booking_view' => 'Просмотр бронирований',
+        'booking_update' => 'Редактирование бронирований',
+        'booking_manage_others' => 'Управление чужими бронированиями',
+
+        'enquiry_view' => 'Просмотр заявок',
+        'enquiry_update' => 'Редактирование заявок',
+        'enquiry_manage_others' => 'Управление чужими заявками',
+
+        'template_view' => 'Просмотр шаблонов',
+        'template_create' => 'Создание шаблонов',
+        'template_update' => 'Редактирование шаблонов',
+        'template_delete' => 'Удаление шаблонов',
+
+        'news_view' => 'Просмотр новостей',
+        'news_create' => 'Создание новостей',
+        'news_update' => 'Редактирование новостей',
+        'news_delete' => 'Удаление новостей',
+        'news_manage_others' => 'Управление чужими новостями',
+
+        'role_manage' => 'Управление ролями',
+        'role_view' => 'Просмотр ролей',
+        'role_create' => 'Создание ролей',
+        'role_update' => 'Редактирование ролей',
+        'role_delete' => 'Удаление ролей',
+
+        'permission_view' => 'Просмотр прав доступа',
+        'permission_create' => 'Создание прав доступа',
+        'permission_update' => 'Редактирование прав доступа',
+        'permission_delete' => 'Удаление прав доступа',
+
+        'dashboard_access' => 'Доступ к панели суперпользователя',
+        'dashboard_baseAdmin_access' => 'Доступ к панели администратора',
+        'dashboard_guest_access' => 'Доступ к панели гостя',
+
+        'settings_update' => 'Редактирование настроек',
+        'settings_view' => 'Просмотр настроек',
+        'settings_delete' => 'Удаление настроек',
+        'settings_manage_others' => 'Управление чужими настройками',
+        'settings_manage_attributes' => 'Управление параметрами настроек',
+
+        'menu_view' => 'Просмотр меню',
+        'menu_create' => 'Создание меню',
+        'menu_update' => 'Редактирование меню',
+        'menu_delete' => 'Удаление меню',
+
+        'user_view' => 'Просмотр пользователей',
+        'user_create' => 'Создание пользователей',
+        'user_update' => 'Редактирование пользователей',
+        'user_delete' => 'Удаление пользователей',
+
+        'page_view' => 'Просмотр страниц',
+        'page_create' => 'Создание страниц',
+        'page_update' => 'Редактирование страниц',
+        'page_delete' => 'Удаление страниц',
+        'page_manage_others' => 'Управление чужими страницами',
+
+        'media_upload' => 'Загрузка медиафайлов',
+        'media_manage' => 'Управление медиафайлами',
+        'media_manage_others' => 'Управление чужими медиафайлами',
+
+        'location_view' => 'Просмотр локаций',
+        'location_create' => 'Создание локаций',
+        'location_update' => 'Редактирование локаций',
+        'location_delete' => 'Удаление локаций',
+        'location_manage_others' => 'Управление чужими локациями',
+
+        'review_manage' => 'Управление отзывами',
+
+        'system_log_view' => 'Просмотр системных логов',
+
+        'theme_manage' => 'Управление темами оформления',
+
+        'social_manage_forum' => 'Управление форумом',
+
+        'plugin_manage' => 'Управление плагинами',
+
+        'vendor_payout_view' => 'Просмотр выплат',
+        'vendor_payout_manage' => 'Управление выплатами',
+
+        'popup_view' => 'Просмотр всплывающих окон',
+        'popup_create' => 'Создание всплывающих окон',
+        'popup_update' => 'Редактирование всплывающих окон',
+        'popup_delete' => 'Удаление всплывающих окон',
+
+        'hotel_view' => 'Просмотр отелей',
+        'hotel_create' => 'Создание отелей',
+        'hotel_update' => 'Редактирование отелей',
+        'hotel_delete' => 'Удаление отелей',
+        'hotel_manage_others' => 'Управление чужими отелями',
+        'hotel_manage_attributes' => 'Управление атрибутами отелей',
+    ]
+];
