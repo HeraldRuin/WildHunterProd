59dee50d8d227b7b5e0d1d28d7461cbf0b4fa7c9
melvin <melvin.melvin.rulit@gmail.com>
2026-05-29 11:39:23 +0300

    добавил дефолтное значение к шапке и подвалу сообщения на почту

--- bc-cms/modules/Email/Views/parts/footer.blade.php
+++ bc-cms/modules/Email/Views/parts/footer.blade.php
@@ -1,5 +1,6 @@
 <div class="b-footer" style="margin-top: 0px;">
     <div class="b-container">
-        {!! setting_item_with_lang('email_footer') !!}
+        @php $email_footer = setting_item_with_lang('email_footer') @endphp
+        {!! $email_footer ? $email_footer : '© 2025' . env('APP_NAME') . __('All rights reserved') !!}
     </div>
 </div>
