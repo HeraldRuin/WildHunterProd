a2b1b8eb45310f9ab0770c73a43667f99c85844b
melvin <melvin.melvin.rulit@gmail.com>
2026-06-04 14:36:09 +0300

    вынес стили таблицы история бронирования в отдельный файл и подключил его

--- bc-cms/modules/Layout/user.blade.php
+++ bc-cms/modules/Layout/user.blade.php
@@ -87,6 +87,7 @@
     </script>
     <link href="{{ asset('dist/frontend/module/user/css/user.css?_ver=' . config('app.asset_version')) }}" rel="stylesheet">
     <link href="{{ asset('dist/frontend/module/user/css/dashboard.css?_ver=' . config('app.asset_version')) }}" rel="stylesheet">
+    <link href="{{ asset('dist/frontend/module/booking/css/bookingHistory.css?_ver=' . config('app.asset_version')) }}" rel="stylesheet">
     <!-- Styles -->
     @stack('css')
     <style type="text/css">
