7bd47fe4c1fc8ea8565ce6e58f5d9154d6768a2b
melvin <melvin.melvin.rulit@gmail.com>
2026-06-10 20:09:21 +0300

    файл перевода

---
+++ bc-cms/resources/lang/ru/hotelAdmin.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'name' => [
+        'model_name' => 'Отели',
+        'user_assign_base_tittle' => 'Привязать пользователя к этой базе',
+        'user_search' => 'Выберите администратора базы из списка',
+    ],
+    'errors' => [
+
+    ],
+    'rules' => [
+
+    ],
+    'validation' => [
+
+    ],
+    'successes' => [
+
+    ]
+];
