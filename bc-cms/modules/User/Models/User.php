55f6e868e3b3df649840f19162e4da15e1a82e5c
melvin <melvin.melvin.rulit@gmail.com>
2026-06-10 20:11:09 +0300

    связь роли к пользователю

--- bc-cms/modules/User/Models/User.php
+++ bc-cms/modules/User/Models/User.php
@@ -1,39 +1,12 @@
 <?php
 namespace Modules\User\Models;
 
-use Modules\Agency\Models\Agency;
-use Modules\Agency\Models\AgencyAgent;
-use Modules\Booking\Models\Booking;
-use Modules\Booking\Models\BookingHunter;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class User extends \App\User
 {
-    /**
-     * @return bool
-     */
-    public function isMasterHunter()
+    public function role(): BelongsTo
     {
-        if (!$this->hasRole('hunter')) {
-            return false;
-        }
-
-        return BookingHunter::where('invited_by', $this->id)
-            ->where('is_master', true)
-            ->exists();
-    }
-
-    /**
-     * Получает все брони, где пользователь является мастер-охотником
-     * (через таблицу bc_booking_hunters)
-     *
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function getMasterHunterBookings()
-    {
-        $bookingHunterIds = BookingHunter::where('invited_by', $this->id)
-            ->where('is_master', true)
-            ->pluck('booking_id');
-
-        return Booking::whereIn('id', $bookingHunterIds);
+        return $this->belongsTo(Role::class,'role_id');
     }
 }
