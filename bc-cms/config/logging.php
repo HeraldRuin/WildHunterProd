241e00e0a0462fa46b048d258a8420719920ced5
melvin <melvin.melvin.rulit@gmail.com>
2026-06-11 13:21:49 +0300

    добавил канал логирования для оплат

--- bc-cms/config/logging.php
+++ bc-cms/config/logging.php
@@ -125,6 +125,12 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'payments' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/payments.log'),
+            'level' => 'info',
+        ],
     ],
 
 ];
