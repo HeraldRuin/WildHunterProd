a1e5ffc20626664f79e7f8164b29d4ce9d7e56c2
melvin <melvin.melvin.rulit@gmail.com>
2026-06-23 11:59:12 +0300

    добавил переводы для кнопок и бокового меню в админке отелей

--- bc-cms/resources/lang/ru/hotelAdmin.php
+++ bc-cms/resources/lang/ru/hotelAdmin.php
@@ -5,6 +5,15 @@ return [
         'user_assign_base_tittle' => 'Привязать пользователя к этой базе',
         'user_search' => 'Выберите администратора базы из списка',
     ],
+    'sidebar_name' => [
+        'hotels' => 'Объекты',
+        'add_new_hotel' => 'Добавить новый объект',
+        'services_hotel' => 'Услуги отеля',
+        'services_room' => 'Услуги в номере',
+    ],
+    'buttons' => [
+        'add_new_service' => 'Добавить услугу',
+    ],
     'errors' => [
 
     ],
