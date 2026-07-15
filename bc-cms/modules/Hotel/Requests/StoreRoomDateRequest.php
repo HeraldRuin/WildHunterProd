dce95d266952736fdff135624c288fc7df667786
melvin <melvin.melvin.rulit@gmail.com>
2026-07-15 11:23:03 +0300

    ошибка сохранения в календаре

--- bc-cms/modules/Hotel/Requests/StoreRoomDateRequest.php
+++ bc-cms/modules/Hotel/Requests/StoreRoomDateRequest.php
@@ -14,7 +14,7 @@ class StoreRoomDateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'target_id' => 'required|integer|exists:hotel_rooms,id',
+            'target_id' => 'required|integer|exists:bc_hotel_rooms,id',
             'start_date' => 'required|date',
             'end_date' => 'required|date|after_or_equal:start_date',
         ];
